4b0b6c2b922f799100d7f7ee55a9732c3b103251
EC2 Default User <ec2-user@ip-172-31-22-88.us-east-2.compute.internal>
2020-02-05 11:43:47 +0000

    20200205アプリ

---
+++ resources/views/admin/garbage/garbagecreate.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+@section('title','収集日通知作成')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h2>収集日通知作成</h2>
+        </div>
+    </div>
+@endsection
